f4832248ab0bab7b45e1ce4110242252122ba4bd
Harsh-Codex8 <harshitgupta8088@gmail.com>
2026-07-30 07:05:08 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -73,4 +73,11 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         });
     });
+    if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('./sw.js')
+            .then(reg => console.log('Service Worker registered successfully!', reg.scope))
+            .catch(err => console.error('Service Worker registration failed:', err));
+    });
+}
 });
