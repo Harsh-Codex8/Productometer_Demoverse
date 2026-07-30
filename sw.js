7b39d6ad049a5b727244933e329c3b079eba0665
Harsh-Codex8 <harshitgupta8088@gmail.com>
2026-07-30 07:05:50 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,31 @@
 const CACHE_NAME = 'productometer-v1';
 const ASSETS_TO_CACHE = [
-    './',
-    './Features/Academics_Monitoring/index.html',
-    './Features/Academics_Monitoring/style.css',
-    './Features/Academics_Monitoring/scripts.js',
-    './manifest.json'
+    './index.html',
+    './style.css',
+    './scripts.js',
+    './manifest.json',
+    './planner-index.html',
+    './Academics_index.html'
+    // Add any other key files or audio/image paths here
+];
+
+// Install Event: Cache all essential files
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(ASSETS_TO_CACHE);
+        })
+    );
+});
+
+// Fetch Event: Serve cached files when offline
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
 ];
 
 self.addEventListener('install', (event) => {
